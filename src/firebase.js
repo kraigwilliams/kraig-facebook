50c0a8b624f4fd46143f6dca9b649dfc78be1a5f
KRAIGYG <williamskrg@gmail.com>
2020-08-27 14:22:00 -0400

    add firebase

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD9SvLGsFHJwj_6ATk7r8yFjzrT2XFu9_Y",
+    authDomain: "kraig-facebook.firebaseapp.com",
+    databaseURL: "https://kraig-facebook.firebaseio.com",
+    projectId: "kraig-facebook",
+    storageBucket: "kraig-facebook.appspot.com",
+    messagingSenderId: "468973515347",
+    appId: "1:468973515347:web:309beeef32bf6d6a0a0ca6",
+    measurementId: "G-EQL685Y1MF"
+  };
+  
