08e345af905ddac8bda67d84167e7e68b7d95da2
KRAIGYG <williamskrg@gmail.com>
2020-08-27 21:40:54 -0400

    to github

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from './Header'
 
 import './App.css';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="app">
       Lets build a facebook clone
+      <Header/>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,28 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import './Header.css';
+import facebook from './images/facebook.png';
+import {Avatar} from '@material-ui/core'
+import SearchIcon from '@material-ui/icons/Search';
 
 function Header() {
     return (
         <div className='header'>
+            <div className='header__left'>
+            <img  className='logo' src={facebook} alt='facebook=logo'/>
+            <SearchIcon/>
+            <input  />
+            </div>
+            <div className='header__middle'>
             
+        </div>
+        <div className='header__right'>
+            <div className='header__info'>
+            <Avatar />
+            
+        </div>
+        
+        </div>
+
         </div>
     )
 }
