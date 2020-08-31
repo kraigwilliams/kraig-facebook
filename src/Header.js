a84b10a4f5ff106eb2b00e6ae82984a780a5d179
KRAIGYG <williamskrg@gmail.com>
2020-08-31 15:53:54 -0400

    import header icons

--- src/Header.js
+++ src/Header.js
@@ -3,6 +3,11 @@ import './Header.css';
 import facebook from './images/facebook.png';
 import {Avatar} from '@material-ui/core'
 import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import FlagIcon from '@material-ui/icons/Flag';
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+import StorefrontOutlinedIcon from '@material-ui/icons/StorefrontOutlined';
+import SubscriptionsOutlinedIcon from '@material-ui/icons/SubscriptionsOutlined';
 
 function Header() {
     return (
@@ -10,6 +15,8 @@ function Header() {
             <div className='header__left'>
             <img  className='logo' src={facebook} alt='facebook=logo'/>
             <SearchIcon/>
+            <HomeIcon/>
+            <FlagIcon/>
             <input  />
             </div>
             <div className='header__middle'>
